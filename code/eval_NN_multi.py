33cb91ba08785c4744bd386011835511108b3eab
janistrov <jan.segessenmann@gmail.com>
2020-05-19 09:18:54 +0200

    com

--- code/eval_NN_multi.py
+++ code/eval_NN_multi.py
@@ -12,15 +12,15 @@ import seaborn as sns
 import numpy as np
 
 ids = []
-titles = ['0', '30', '60', '90', '120']
+titles = ['0', '30', '90', '120', '150']
 for i in range(len(titles)):
-    ids.append('Hidden_size_' + str(titles[i]))
+    ids.append('model_hidden_' + str(titles[i]))
 
 
 util.plot_multi_boxplots(ids, x='hidden_size', y='correlation', hue='train_set')
 util.plot_multi_boxplots(ids, x='hidden_size', y='mse', hue='train_set')
 util.plot_multi_boxplots(ids, x='hidden_size', y='mae', hue='train_set')
-util.plot_multi_scatter(ids, save_name='hidden_size')
+util.plot_multi_scatter(ids, save_name='hidden_size_sigmoid')
 
 
 
--- code/eval_NN_single.py
+++ code/eval_NN_single.py
@@ -13,35 +13,15 @@ import numpy as np
 
 #id = 'model_lambda_0.1'
 ids = []
-titles = ['0', '30', '60', '90', '120']
+titles = ['W']
 for i in range(len(titles)):
-    ids.append('Hidden_size_' + str(titles[i]))
+    ids.append('model_small_' + str(titles[i]))
 
-"""
-util.plot_optimization(id)
-"""
-for i, id in enumerate(ids):
-    util.make_prediction(id)
-    util.make_distances(id)
-
-"""
-df = pd.DataFrame(util.make_distances(id, train_set=True))
-print('---------')
-print('Mean Corr.: ' + str(df['correlation'].mean()))
-print('Mean MSE.: ' + str(df['mse'].mean()))
-print('Mean MAE.: ' + str(df['mae'].mean()))
-
-
-df = pd.DataFrame(util.make_distances(id))
-print('---------')
-print('Mean Corr.: ' + str(df['correlation'].mean()))
-print('Mean MSE.: ' + str(df['mse'].mean()))
-print('Mean MAE.: ' + str(df['mae'].mean()))
-
-
-util.plot_prediction(id, [5, 26, 46, 54], lim_nr_samples=2000)
-
-util.plot_weights(id, linewidth=0)
-
-
-"""
+for i, val in enumerate(ids):
+    util.plot_optimization(val)
+    util.make_prediction(val)
+    util.make_distances(val)
+    #util.make_distances(val, train_set=True)
+    #util.plot_prediction(val, [5, 26, 46, 54], lim_nr_samples=2000)
+    util.plot_weights(val, linewidth=0)
+    print(f'id: {val} done')

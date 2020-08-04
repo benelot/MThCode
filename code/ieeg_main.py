12175818daef395fb8fef7b37653a043cadf4288
Jan Segessenmann <jan.segessenmann@gmail.com>
2020-08-04 16:28:33 +0200

    ready for sample size estim.

--- code/ieeg_main.py
+++ code/ieeg_main.py
@@ -74,7 +74,7 @@ patient_id = ['ID07', 'ID07', 'ID07', 'ID08', 'ID08', 'ID08', 'ID11', 'ID11', 'I
 time_begin = [[32, 0], [35, 0], [38, 0], [58, 0], [60, 0], [64, 30], [60, 0], [62, 0], [65, 0], [129, 40],
               [132, 30], [136, 30]]
 
-determine_sample_size(patient_id, time_begin, max_sample_size=60, dt=5)
+determine_sample_size(patient_id, time_begin, max_sample_size=10*60, dt=5)
 
 
 # ieeg.plot_corr_connectivity(r2, r2_dt, h2, h2_dt, save_name=name)

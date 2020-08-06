d2d6af3ff7ce5983d0c9d2a8660bf50444db2a8f
Jan Segessenmann <jan.segessenmann@gmail.com>
2020-08-06 19:05:41 +0200

    tau plots revised

--- code/ieeg_main.py
+++ code/ieeg_main.py
@@ -1,15 +1,14 @@
-
 import ieeg_utilities as ieeg
 
 patient_id = ['ID07', 'ID07', 'ID07', 'ID08', 'ID08', 'ID08', 'ID11', 'ID11', 'ID11', 'ID11', 'ID11', 'ID11']
 time_begin = [[32, 7], [35, 15], [38, 22], [57, 58], [60, 10], [64, 40], [60, 5], [62, 12], [65, 0], [129, 48],
               [132, 35], [136, 35]]
-duration = 5
+duration = 60
 
 for i in range(len(patient_id)):
     print(str(i))
-    cc, tl, tau, cctl = ieeg.lin_corr(patient_id=patient_id[i], time_begin=time_begin[i], duration=duration)
-    ieeg.lin_corr_plot(cc=cc, tl=tl, tau=tau, patient_id=patient_id[i], time_begin=time_begin[i])
+    ieeg.lin_corr(patient_id=patient_id[i], time_begin=time_begin[i], duration=duration)
+    break
 
 
 
